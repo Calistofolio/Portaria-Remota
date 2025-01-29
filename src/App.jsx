d4e29c9416b7875740cb8d839e9dbcbcafccba19
Luana Calisto <luanavenancio060@gmail.com>
2025-01-28 22:52:29 -0300

    create visit context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/home'
 import VisitData from './pages/visitData/visitData'
+import { VisitContext } from './context/visitContext'
 
 function App() {
 
   return (
-    <Routes>
-      <Route index element={<Home/>}/>
-      <Route path='/visitData' element={<VisitData/>}/>
-    </Routes>
+    <VisitContext>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='/visitData' element={<VisitData/>}/>
+      </Routes>
+    </VisitContext>
   )
 }
 
